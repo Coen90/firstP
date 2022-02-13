6fe92a76c4ef823062f61e31d40d7fe23b37c7da
Hyuntae <bht9011@gmail.com>
2022-02-13 23:04:29 +0900

    2022-02-13 commit before sleep

---
+++ src/main/java/com/coen/firstP/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.coen.firstP.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}

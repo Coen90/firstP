8f92d47e28a9b0b3c5a1dc55187fe67410782920
Hyuntae <bht9011@gmail.com>
2022-02-13 12:19:16 +0900

    2022-02-13 commit

---
+++ src/main/java/com/coen/firstP/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.coen.firstP.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
